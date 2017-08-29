a8d41bd807a9b2e963a6a7d8a4287156d7705e26
josevaisman <hellovaisman@gmail.com>
2017-08-29 16:20:49 -0300

    test

---
+++ resources/themes/bootstrap/default_footer.php
@@ -0,0 +1,7 @@
+<hr>
+
+<div class="footer">
+    Templates para <a href="http://www.finhub.cl/index.php">
+     <img src="<?php echo THEMEPATH; ?>/img/logo_finhub.png" height="20" style="vertical-align: text-bottom;" alt="finhub"></img>
+     </a>
+</div>
