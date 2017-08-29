119d7c117381a456ba475513d7e825172aef1d3c
josevaisman <hellovaisman@gmail.com>
2017-08-29 15:39:01 -0300

    Limpieza + Browser

---
+++ resources/themes/bootstrap/default_footer.php
@@ -0,0 +1,7 @@
+<hr>
+
+<div class="footer">
+    Templates para <a href="http://www.lvaindices.com">
+     <img src="<?php echo THEMEPATH; ?>/img/logo.png" height="20" style="vertical-align: text-bottom;" alt="lvaindices"></img>
+     </a>
+</div>
