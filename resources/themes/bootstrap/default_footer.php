5d9a701cca45a4059ee3d54204729cbe4bbff73b
josevaisman <hellovaisman@gmail.com>
2017-08-29 15:40:45 -0300

    Test

--- resources/themes/bootstrap/default_footer.php
+++
@@ -1,7 +0,0 @@
-<hr>
-
-<div class="footer">
-    Templates para <a href="http://www.lvaindices.com">
-     <img src="<?php echo THEMEPATH; ?>/img/logo.png" height="20" style="vertical-align: text-bottom;" alt="lvaindices"></img>
-     </a>
-</div>
